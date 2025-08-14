64d2709b38f0693bb5c95da1f0d52eea0f7fc04b
giulista11 <giulista11@gmail.com>
2025-08-14 13:45:34 -0300

    se agrego control de campos vacios

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,22 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+const form = document.getElementById('regBtn');
+
+form.addEventListener('click', function() {
+    let campos = document.getElementsByClassName("form-control");
+    let hayVacio = false;
+
+    for (let i = 0; i < campos.length; i++) {
+        if (campos[i].value.trim() === '') {
+            hayVacio = true;
+        }
+    }
+
+    if (hayVacio) {
+        showAlertError();
+    } else {
+        showAlertSuccess();
+    }
+});
