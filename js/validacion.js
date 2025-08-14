af849047ad269fe9915d152ea5580c0f7262f60f
Valentin Silva <valentinvsilva@gmail.com>
2025-08-14 13:53:03 -0300

    password comprovation

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,21 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+
+document.getElementById('regBtn').addEventListener('click', function(e){
+
+
+})
+
+
+function comprobarPasswords() {
+    let password1 = document.getElementById('password1').value
+    let password2 = document.getElementById('password2').value
+console.log(password1)
+console.log(password1 == password2)
+return password1 == password2
+
+
+}
